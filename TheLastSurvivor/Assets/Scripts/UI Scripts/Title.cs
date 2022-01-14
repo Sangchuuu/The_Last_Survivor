7dbd7d81ead9905cd5c81ce6e88149321ee15b1b
Sangchuuu <97686315+Sangchuuu@users.noreply.github.com>
2022-01-14 21:40:23 +0900

    Ui셋팅
    ㅇ

---
+++ TheLastSurvivor/Assets/Scripts/UI Scripts/Title.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Title : MonoBehaviour
+{
+    [SerializeField]
+    Button m_objStartButton;
+
+}
