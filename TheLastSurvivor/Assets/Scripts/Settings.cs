d660cb57b97582d8e41a8733bf1919094d2df4b1
zetite77 <64113427+zetite77@users.noreply.github.com>
2022-01-16 00:59:53 +0900

    세팅 UI기본

---
+++ TheLastSurvivor/Assets/Scripts/Settings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas m_Cvs_Settings;
+
+    public Button m_Label_Vibration;
+    public Scrollbar m_ScrollBar_Vibration;
+    public Text m_ScrollBar_Text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_ScrollBar_Vibration.value >= 0.5f)
+            m_ScrollBar_Text.text = "OFF";
+        else
+            m_ScrollBar_Text.text = "ON";
+    }
+}
