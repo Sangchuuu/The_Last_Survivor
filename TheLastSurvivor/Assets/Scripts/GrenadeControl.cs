5238f92c6b26830b13739f9b2658adca0cf3d1ba
zetite77 <64113427+zetite77@users.noreply.github.com>
2022-01-17 02:29:23 +0900

    1.17 2:29 수류탄 중간점검

---
+++ TheLastSurvivor/Assets/Scripts/GrenadeControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GrenadeControl : MonoBehaviour
+{
+    private Button m_GrenadeButton;
+    public Image m_GrenadeRange;
+    public GameObject m_StartPoint;
+
+    public float rangeSize;
+
+    private void Start()
+    {
+        m_GrenadeButton = this.GetComponent<Button>();
+        m_GrenadeButton.onClick.AddListener(GrenadeButtonClick);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            m_GrenadeRange.gameObject.SetActive(true);
+            Vector3 mousePos = Input.mousePosition;
+            m_GrenadeRange.transform.localPosition = mousePos*2.3f;  // << 이게 왜 x2.3해야됨??
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            m_GrenadeRange.gameObject.SetActive(false);
+        }
+
+    }
+
+    void GrenadeButtonClick()
+    {
+        m_GrenadeRange.transform.position = m_StartPoint.transform.position;
+        m_GrenadeRange.gameObject.SetActive(true);
+    }
+}
