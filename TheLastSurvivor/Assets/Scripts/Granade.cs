321ab6a82273c08d5d1ae614f3e67f537be03ce2
Sangchuuu <97686315+Sangchuuu@users.noreply.github.com>
2022-01-17 14:41:05 +0900

    수류탄테스트(22.01.17)
    테스트

---
+++ TheLastSurvivor/Assets/Scripts/Granade.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class Granade : MonoBehaviour
+{
+    public GameObject m_objGrenadeOffBtn;
+    public GameObject m_objGrenadeOnBtn;
+    public GameObject m_objTempShelter;
+    public GameObject m_objGrenade;
+
+    public int m_nDamage;
+    public int m_nCount;
+    
+    Vector2 MousePosition;
+    public Camera camera;
+
+    public void SetGrenadeOn()
+    {
+        if(m_objGrenadeOnBtn.activeSelf == false)
+        {
+            m_objGrenade.transform.position = m_objTempShelter.transform.position;
+        }
+    }
+    public void MouseLocate()
+    {
+
+    }
+  
+
+    void Update()
+    {
+        SetGrenadeOn();
+
+    }
+}
