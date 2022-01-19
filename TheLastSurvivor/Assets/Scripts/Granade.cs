f3ec30aeb89b7b11b3d98dbcba4e0caef13d6c17
Sangchuuu <97686315+Sangchuuu@users.noreply.github.com>
2022-01-19 12:03:47 +0900

    수류탄 완성..(2022.01.19)
    힘들었따.ㅎ

--- TheLastSurvivor/Assets/Scripts/Granade.cs
+++ TheLastSurvivor/Assets/Scripts/Granade.cs
@@ -13,23 +13,44 @@ public class Granade : MonoBehaviour
     public int m_nDamage;
     public int m_nCount = 3;
 
+
     public bool m_bGrenade = false;
 
-   
-    public void OnMouseDrag()
+    CircleCollider2D collider2D;
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (m_objGrenadeOffBtn.activeSelf == true)
+        if(other.gameObject.tag == "Player")
         {
-            
-            Vector3 mouseposition = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
-            Vector3 objposition = Camera.main.ScreenToWorldPoint(mouseposition);
-            transform.position = objposition;
+            Debug.Log("충돌");
+            this.gameObject.SetActive(false);
         }
     }
    
-
     void Update()
     {
+        collider2D = this.gameObject.GetComponent<CircleCollider2D>();
+        if (m_bGrenade == false)
+        {
+            if (m_objGrenadeOffBtn.activeSelf == true)
+            {
+                transform.position = m_objTempShelter.transform.position;
+                m_bGrenade = true;
+            }
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mouseposition = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+            Vector3 objposition = Camera.main.ScreenToWorldPoint(mouseposition);
+            objposition.z = 0; 
+            transform.position = objposition;
+            collider2D.enabled = false;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            collider2D.enabled = true;
+            
+        }
 
 
     }
